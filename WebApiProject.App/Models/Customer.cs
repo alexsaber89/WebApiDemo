698910f67cb8c748d604d699b6612fc0ebe0e14f
Alex Saber <alexsaber89@gmail.com>
2017-04-19 17:43:18 -0500

    initial commit with two Model implementations

---
+++ WebApiProject.App/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiProject.App.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+    }
+}
