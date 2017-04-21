4a27cba5c18f2d54fae55ba19272550e55603317
Alex Saber <Alex Saber>
2017-04-21 18:08:51 -0500

    add HttpGet methods for Customer and Product controllers

--- WebApiProject.App/Controllers/CustomerController.cs
+++ WebApiProject.App/Controllers/CustomerController.cs
@@ -4,10 +4,37 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApiProject.App.DAL;
 
 namespace WebApiProject.App.Controllers
 {
     public class CustomerController : ApiController
     {
+        readonly ICustomerRepo _customerRepo;
+
+        public CustomerController()
+        {
+            _customerRepo = new CustomerRepo();
+        }
+
+        public CustomerController(ICustomerRepo customerRepo)
+        {
+            _customerRepo = customerRepo;
+        }
+
+        [HttpGet]
+        [Route("api/customer/{id}")]
+        public HttpResponseMessage HttpGetCustomerById(int id)
+        {
+            var customer = _customerRepo.GetCustomerById(id);
+
+            if (customer == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                    $"The Customer with an id of {id} does not exist");
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, customer);
+        }
     }
 }
--- WebApiProject.App/Controllers/ProductController.cs
+++ WebApiProject.App/Controllers/ProductController.cs
@@ -4,10 +4,37 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApiProject.App.DAL;
 
 namespace WebApiProject.App.Controllers
 {
     public class ProductController : ApiController
     {
+        readonly IProductRepo _productRepo;
+
+        public ProductController()
+        {
+            _productRepo = new ProductRepo();
+        }
+
+        public ProductController(IProductRepo productRepo)
+        {
+            _productRepo = productRepo;
+        }
+
+        [HttpGet]
+        [Route("api/product/{id}")]
+        public HttpResponseMessage HttpGetProductById(int id)
+        {
+            var product = _productRepo.GetProductById(id);
+
+            if (product == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                    $"The Product with an id of {id} does not exist");
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, product);
+        }
     }
 }
